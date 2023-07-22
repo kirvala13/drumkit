65287ac2219c61a0f5d500ff372570ecb6c0c5d4
kirvala13 <74554775+kirvala13@users.noreply.github.com>
2023-07-22 18:26:47 +0400

    drum

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("keydown", (e)=>{
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if(!audio) return
+  audio.currentTime = 0;
+  audio.play()
+ key.classList.add("playing")
+})
+
+const keys = document.querySelectorAll(".key")
+function removetrans(e){
+ if(e.propertyName !== "transform") return;
+ this.classList.remove("playing");
+}
+keys.forEach(e => e.addEventListener("transitionend", removetrans));
